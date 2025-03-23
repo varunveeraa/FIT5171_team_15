2910b80275a1dde7a41b0983d575e82e3a214a09
Varun <vvee0008@student.monash.edu>
2025-03-23 13:37:40 +1100

    Test 3 - TestBuyTicketTransferInvalid

---
+++ src/target/test-classes/fit5171/monash/TestBuyTicketTransferInvalid.java
@@ -0,0 +1,12 @@
+package fit5171.monash.edu;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class TestBuyTicketTransferInvalid {
+    @Test
+    public void testBuyTicketWithTransferInvalidIds() {
+        TicketSystem ticketSystem = new TicketSystem();
+        assertDoesNotThrow(() -> ticketSystem.buyTicket(111, 222));
+    }
+}
